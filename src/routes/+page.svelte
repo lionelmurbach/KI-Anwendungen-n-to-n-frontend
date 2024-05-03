276c34f6678aaf66b2caee17e498e27fcc4210ac
Lionel Murbach <murbalio@students.zhaw.ch>
2024-05-03 16:14:27 +0200

    chore: Update background image URL and add styling to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,25 @@
 <p>For any questions, please contact me via email:</p>
 <p>murbalio@students.zhaw.ch</p>
 
+<div class="background"></div>
+
+<style>
+    .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url('https://imgur.com/j6PLFcd.png');
+        background-size: cover;
+        filter: brightness(100%) blur(10px); 
+        z-index: -1; 
+    }
+
+    .content {
+        position: relative;
+        z-index: 1;
+        padding: 20px;
+        color: white;
+    }
+</style>
